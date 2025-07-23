c2f807e99dc58885973b984a41b945a92e3ee548
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-23 11:19:35 +0530

    Implemented parsing the single number strings

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,7 @@ class StringCalculator {
   int add(String input) {
     // If the input is empty, return 0.
     if (input.isEmpty) return 0;
-    return -1;
+    // Parse the string to int for single number strings.
+    return int.parse(input);
   }
 }
