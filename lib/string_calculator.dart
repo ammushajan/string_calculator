46563e0db42d352476bc348939eaabda977bde8f
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-23 12:03:15 +0530

    Handled custom delimitter in the input string

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,8 +3,19 @@ class StringCalculator {
   int add(String input) {
     // If the input is empty, return 0.
     if (input.isEmpty) return 0;
+    // Check for custom delimiter
+    String delimiter = ',|\n';
+    // If the input starts with a custom delimiter, extract it.
+    if (input.startsWith('//')) {
+      final parts = input.split('\n');
+      // Get delimiter from "//;\n", considering ; as a custom delimiter
+      delimiter = RegExp.escape(parts[0].substring(2));
+      // Use the rest of the string as input
+      input = parts[1];
+    }
+
     // Split the string by comma or newline
-    List<String> numberStrings = input.split(RegExp(r'[,\n]'));
+    List<String> numberStrings = input.split(RegExp(delimiter));
     // If the input contains multiple numbers, return the sum of those numbers.
     return numberStrings.map(int.parse).reduce((a, b) => a + b);
   }
