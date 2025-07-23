79bad9356be12f17bf654daceaf5e8f2092755fd
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-23 12:50:28 +0530

    extracted helper function for delimiter,parsing and to check negative numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,7 +3,22 @@ class StringCalculator {
   int add(String input) {
     // If the input is empty, return 0.
     if (input.isEmpty) return 0;
-    // Check for custom delimiter
+    // Get delimiters and numbers from the input string.
+    final delimitersAndNumbers = getDelimitersAndNumbers(input);
+    String delimiter = delimitersAndNumbers['delimiter']!;
+    input = delimitersAndNumbers['input']!;
+    // Split the string by the delimiter.
+    List<String> numberList = input.split(RegExp(delimiter));
+    // Check for negative numbers and throw an exception if any are found.
+    _checkForNegativeNumbers(numberList);
+    // Convert the string numbers to integers and return their sum.
+    return numberList.map(int.parse).reduce((a, b) => a + b);
+  }
+
+  ///[getDelimitersAndNumbers] extracts delimiters and numbers from the input string.
+  /// It supports both default delimiters (comma and newline) and custom delimiters.
+  /// Returns a map with 'delimiter' and 'input' keys.
+  Map<String, String> getDelimitersAndNumbers(String input) {
     String delimiter = ',|\n';
     // If the input starts with a custom delimiter, extract it.
     if (input.startsWith('//')) {
@@ -13,16 +28,17 @@ class StringCalculator {
       // Use the rest of the string as input
       input = parts[1];
     }
+    return {'delimiter': delimiter, 'input': input};
+  }
 
-    // Split the string by the delimiter.
-    List<String> numberList = input.split(RegExp(delimiter));
+  ///[_checkForNegativeNumbers] checks for negative numbers in the list.
+  /// If any negative numbers are found, an exception is thrown listing them.
+  void _checkForNegativeNumbers(List<String> numbers) {
     // Get negative numbers
-    final negatives = numberList.where((n) => int.parse(n) < 0).toList();
+    final negatives = numbers.where((n) => int.parse(n) < 0).toList();
     // If there are negative numbers, throw an exception listing them.
     if (negatives.isNotEmpty) {
       throw Exception('negative numbers not allowed ${negatives.join(',')}');
     }
-    // Convert the string numbers to integers and return their sum.
-    return numberList.map(int.parse).reduce((a, b) => a + b);
   }
 }
